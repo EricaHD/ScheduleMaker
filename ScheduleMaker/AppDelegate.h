f0eeb0b86f012200f526546fbf1f47ab00e5e2f0
Erica Dominic <erica.h.dominic@gmail.com>
2017-03-13 09:06:11 -0400

    Commit before changing "special stations" table cell to be an adjustable list with +/-.

--- ScheduleMaker/AppDelegate.h
+++ ScheduleMaker/AppDelegate.h
@@ -6,16 +6,23 @@
 //  Copyright © 2016 Erica Dominic. All rights reserved.
 //
 
-// ADDITIONS:
-// 1. AUTOCOMPLETION for staff entry!!!  But would then need the ability to view staff "dictionary," and also add/delete/modify entries within it
-// 2. README/instruction booklet describing use of software
-// 3. Further error checking?  Like entering only a space " " will count as a valid entry
+// TO DO NEXT:
+// Make special stations customizable with +/- buttons
+// Rearrange rows on Requirements view controller
+// Lay out Database view controller
+// Learn how to collate and/or pass data between three view controllers
+// Add autocomplete from database feature in Schedule view controller (for names and specific stations)
+// Actually do the data processing and making of the schedule
+// Invoke a script that will open LibreOffice and fill in schedule as if Sook has just written it, ready for editting (if necessary) and printing as usual
 
-// POSSIBLE ADDITIONS:
-// 4. Could add lunch options to bottom section, underneath "half hours" (e.g. "backload lunches," "hour lunches if possible")
-// 5. Could display messages on the results, i.e. "Gallery must be closed 12-1 pm" or "Only one person stationed on floor -1 during 11-5 pm"
-// 6. Possibly include fireguard status and follow the 2 fireguard rule (?)
-// 7. Schedule could specify different stations for floor 0 given suffiecient staff (e.g. HYHY, MOSC)
+// ADDITIONS:
+// 1. README/instruction booklet describing use of software
+// 2. Further error checking?  Like entering only a space " " will count as a valid entry
+// 3. Could adds "lunch" or "no lunch" option in case someone wants a lunch but is working <5.5 hours
+// 4. Could display messages on the results, i.e. "Gallery must be closed 12-1 pm" or "Only one person stationed on floor -1 during 11-5 pm"
+// 5. Schedule could specify different stations for floor 0 given sufficient staff (e.g. HYHY, MOSC)
+
+// Note: go back to "TODO" and "DEFAULT"
 
 #import <Cocoa/Cocoa.h>
 
