da87190832fda01d64c3bd99bd4f0f82eb7b4068
Erica Dominic <erica.h.dominic@gmail.com>
2017-03-24 00:08:02 -0400

    Adjusted goals list in AppDelegate.h.  Ready for the next step, whatever that may be.

--- ScheduleMaker/AppDelegate.h
+++ ScheduleMaker/AppDelegate.h
@@ -6,19 +6,19 @@
 //  Copyright © 2016 Erica Dominic. All rights reserved.
 //
 
-// MAJOR MILESTONES:
-// Add another "0" and "Float" to RequirementsViewController?
-// Make special stations customizable with +/- buttons
-// Actually do the data processing and making of the schedule in Model.m (neaten up while you're at it)
-// Invoke a script that will open LibreOffice and fill in schedule as if Sook has just written it, ready for editing (if necessary) and printing as usual
-// Lay out Database view controller
-// Add autocomplete from database feature in Schedule view controller (for names AND specific stations)
-// Revisit DEFAULTs flags and actually implement NSUserDefaults
+// MAJOR GOALS:
+// 1.1 Add another "0" and one or two "Float"s to RequirementsViewController list?
+// 1.2 Lay out Database view controller
+// 1.3 Add autocomplete from database feature in Schedule view controller (for names AND specific stations)
+// 2.1 Actually do the data processing and making of the schedule in Model.m (neaten up while you're at it)
+// 2.2 Invoke a script that will open LibreOffice and fill in schedule as if Sook has just written it, ready for editing (if necessary) and printing as usual
+// 3.1 Revisit DEFAULTs flags and actually implement NSUserDefaults
+// 4.1 Make special stations customizable with +/- buttons
 
 // EVENTUAL ADDITIONS:
 // README/instruction booklet describing use of software
 // Further error checking?  Like entering only a space " " will count as a valid entry
-// Could adds "lunch" or "no lunch" option in case someone wants a lunch but is working <5.5 hours
+// Could adds "lunch" or "no lunch" option in case someone wants a lunch but is working < 5.5 hours
 // Could display messages on the results, i.e. "Gallery must be closed 12-1 pm" or "Only one person stationed on floor -1 during 11-5 pm"
 // Schedule could specify different stations for floor 0 given sufficient staff (e.g. HYHY, MOSC)
 // Go back and figure out which @property need (strong) or other qualifiers
