2c1ad650901030511d40cb2b1a3bbec2727ecffb
Erica Dominic <erica.h.dominic@gmail.com>
2017-03-26 14:30:56 -0400

    Laid out add/edit staff view.

---
+++ ScheduleMaker/AddEditStaffViewController.h
@@ -0,0 +1,13 @@
+//
+//  AddEditStaffViewController.h
+//  ScheduleMaker
+//
+//  Created by Erica Dominic on 3/26/17.
+//  Copyright © 2017 Erica Dominic. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AddEditStaffViewController : NSViewController
+
+@end
---
+++ ScheduleMaker/AddEditStaffViewController.m
@@ -0,0 +1,25 @@
+//
+//  AddEditStaffViewController.m
+//  ScheduleMaker
+//
+//  Created by Erica Dominic on 3/26/17.
+//  Copyright © 2017 Erica Dominic. All rights reserved.
+//
+
+#import "AddEditStaffViewController.h"
+
+@interface AddEditStaffViewController ()
+
+@end
+
+@implementation AddEditStaffViewController
+
+// Do view setup here
+- (void)viewDidLoad {
+
+	// Super
+	[super viewDidLoad];
+
+}
+
+@end
